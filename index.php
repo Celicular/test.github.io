c37ec65e008336c02a0d24e5a1066d1f9444aa50
Himadri <119931990+Celicular@users.noreply.github.com>
2023-06-01 19:18:13 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
-<?php
-echo "Himadri";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP HTML Example</title>
+</head>
+<body>
+    <h1>Hello, PHP!</h1>
+    <p>This is a simple HTML code embedded in a PHP file.</p>
+    
+    <?php
+        // PHP code can be written here
+        $name = "John";
+        echo "<p>Welcome, $name!</p>";
+    ?>
+</body>
+</html>
